2c44421d4157181d5181d547750e6f5c1288d659
Naveen B Kulkarni <naveenkulkarni029@gmail.com>
2020-02-24 12:20:28 +0530

    added steuth and logger

--- src/main/java/org/nbk/movies/controller/MoviesController.java
+++ src/main/java/org/nbk/movies/controller/MoviesController.java
@@ -1,6 +1,8 @@
 package org.nbk.movies.controller;
 
 import org.nbk.movies.domain.Movie;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MoviesController {
+    
+    public static final Logger LOGGER = LoggerFactory.getLogger(MoviesController.class);
 
 	@GetMapping(value="api/v1/movies", produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Movie> getMovies(){
+	    
+	    LOGGER.info("get Movie");
+	    
 		Movie movie = new Movie();
 		movie.setMovieName("3Idiots :P");
 		
